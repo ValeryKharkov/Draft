78455f713402e2128a02797f2b8abb144bd0500f
ValeryKharkov <valery@example.com>
2025-03-02 23:18:33 +0300

    course hyperskill: fix Draft

---
+++ src/course/hyperskill/Draft.java
@@ -0,0 +1,17 @@
+package course.hyperskill;
+
+public class Draft {
+
+    public static int convertToInt(double value) {
+        return (int) value;
+    }
+
+    public static void main(String[] args) {
+        double input = 6.75;
+        // Insert the correct method call here
+        int result = convertToInt(input); System.out.println(result);
+        System.out.println(convertToInt(input));
+
+
+    }
+}
