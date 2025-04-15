07772f7e22818db8366fba95a428a7f503f2c419
ValeryKharkov <valery@example.com>
2025-04-15 23:42:06 +0300

    repository: fix class StudentRepositoryImpl

--- src/repository/StudentRepositoryImpl.java
+++ src/repository/StudentRepositoryImpl.java
@@ -5,33 +5,35 @@ import java.util.HashMap;
 /**
  * Класс для реализации методов, определенных в интерфейсе StudentRepository
  */
-public class StudentRepositoryImpl implements StudentRepository
-{
+public class StudentRepositoryImpl implements StudentRepository {
+
     private HashMap<Integer, Student> students;
 
     public StudentRepositoryImpl()
     {students = new HashMap<>();}
 
     @Override
-    public Student getStudentById(int id)
-    {return students.get(id);}
-
-    @Override
-    public void addStudent(Student s)
-    {students.put(s.getId(), s);}
-
-    @Override
-    public void updateStudent(Student s)
-    {
-        (students.get(s.getId())).setName(s.getName());
-        (students.get(s.getId())).setEmail(s.getEmail());
+    public Student getStudentById(int id) {
+        return null;
     }
 
     @Override
-    public void deleteStudent(int id)
-    {students.remove(id);}
+    public void addStudent(Student s) {
+
+    }
 
     @Override
-    public boolean exists(int id)
-    {return students.containsKey(id);}
+    public void updateStudent(Student s) {
+
+    }
+
+    @Override
+    public void deleteStudent(int id) {
+
+    }
+
+    @Override
+    public boolean exists(int id) {
+        return false;
+    }
 }
